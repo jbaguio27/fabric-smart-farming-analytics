8c3400d4b4869a39da851caf9bf453de8994a4c6
Joseph Baguio <ioseph.ab@gmail.com>
2026-08-26 12:39:44 +0800

    Committing 1 item from workspace 92bc9c4b-1186-473f-8398-f198e8b16b45

--- fabric/SmartFarming_Warehouse.Warehouse/dbo/Tables/dim_crop.sql
+++ fabric/SmartFarming_Warehouse.Warehouse/dbo/Tables/dim_crop.sql
@@ -6,6 +6,8 @@ CREATE TABLE [dbo].[dim_crop] (
 	[optimal_humidity_percent] float NULL, 
 	[target_biomass_g] float NULL, 
 	[harvest_cycle_days] int NULL, 
+	[unit_price_grade_a_php] float NULL, 
+	[unit_price_grade_b_php] float NULL, 
 	[created_timestamp] datetime2(6) NULL, 
 	[pipeline_run_date] date NULL
 );
