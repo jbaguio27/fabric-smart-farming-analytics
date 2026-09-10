75c4819d9ecfe3de8a6ffd15e693c3521e8d1b81
jbaguio27 <ioseph.ab@gmail.com>
2026-09-11 01:44:45 +0800

    fix(warehouse): align fact_dataops_pipeline_log column types to VARCHAR(8000) and BIGINT to prevent dacpac data-loss conflict

--- fabric/SmartFarming_Warehouse.Warehouse/dbo/Tables/fact_dataops_pipeline_log.sql
+++ fabric/SmartFarming_Warehouse.Warehouse/dbo/Tables/fact_dataops_pipeline_log.sql
@@ -1,14 +1,14 @@
 CREATE TABLE [dbo].[fact_dataops_pipeline_log] (
-    [TraceId]             VARCHAR (100)  NULL,
-    [SpanId]              VARCHAR (50)   NULL,
-    [PipelineName]        VARCHAR (100)  NULL,
-    [StageName]           VARCHAR (100)  NULL,
-    [Component]           VARCHAR (100)  NULL,
-    [ExecutionStatus]     VARCHAR (50)   NULL,
+    [TraceId]             VARCHAR (8000) NULL,
+    [SpanId]              VARCHAR (8000) NULL,
+    [PipelineName]        VARCHAR (8000) NULL,
+    [StageName]           VARCHAR (8000) NULL,
+    [Component]           VARCHAR (8000) NULL,
+    [ExecutionStatus]     VARCHAR (8000) NULL,
     [SourceRowCount]      BIGINT         NULL,
     [TargetRowCount]      BIGINT         NULL,
-    [ExecutionDurationMs] INT            NULL,
-    [ErrorMessage]        VARCHAR (1000) NULL,
+    [ExecutionDurationMs] BIGINT         NULL,
+    [ErrorMessage]        VARCHAR (8000) NULL,
     [Timestamp]           DATETIME2 (6)  NULL
 );
 
